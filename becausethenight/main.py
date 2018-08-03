e9055b1ef213ae7f5a128d87244cb992b61a475e
devedzic <devedzic@gmail.com>
2018-08-03 23:45:45 +0200

    Initial commit. Just a few prints. Unstructured.

---
+++ becausethenight/main.py
@@ -0,0 +1,20 @@
+# Hello World
+
+# print("Because the Night")
+# print()
+# print('Because the night ' + '\n' + 'belongs to lovers')
+
+
+# Hello World elaborated a bit, then moved to getting_started.beginning.py
+
+def print_it():
+    print("Because the Night")
+    print()
+    print('Because the night ' + '\n' + 'belongs to lovers')
+
+print_it()
+print()
+
+for i in range(1, 5):
+    print(i, " Because the Night")
+
