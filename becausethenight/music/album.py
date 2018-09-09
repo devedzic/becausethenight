fe1a3684e78df393b227d1da39114b70430b4f54
devedzic <devedzic@gmail.com>
2018-09-09 21:09:52 +0200

    Iterators and generators introduced in author.py.

--- becausethenight/music/album.py
+++ becausethenight/music/album.py
@@ -18,6 +18,8 @@ class Album:
         self.duration = duration
         self.release_date = release_date
 
+        self.__i = 0                                            # iterator index
+
     def __str__(self):
         return (self.title + '\n' +
                 '\t' + 'performer(s): ' + utility.format_performer(self.performer) + '\n' +
@@ -34,6 +36,16 @@ class Album:
         else:
             return False
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if (self.__i + 1) <= len(self.songs):
+            self.__i += 1
+            return self.songs[self.__i - 1]
+        else:
+            raise StopIteration
+
 
 def format_songs(songs):
     """Formats the album's song list for __str__()."""
@@ -42,6 +54,13 @@ def format_songs(songs):
         return '\t\t' + '\n\t\t'.join(song.title for song in songs)
     else:
         return '\t\t' + 'not specified'
+
+
+def generate_songs(songs):
+    i = 0
+    while i < len(songs):
+        yield songs[i]
+        i += 1
 
 
 if __name__ == "__main__":
@@ -80,12 +99,53 @@ if __name__ == "__main__":
     print(format_songs(songs))
     print()
 
-    easter = Album('Easter')                                    # test formatting an Album object
-    print(easter)
-    easter = Album('Easter', pattiSmithGroup, songs, 2800, date(1978, 3, 2))
-    print(easter)
-
     # for song in songs:
     #     print(song)
     # print()
 
+    easter = Album('Easter')                                    # test formatting an Album object
+    print(easter)
+    easter = Album('Easter',
+                   pattiSmithGroup,
+                   songs,
+                   2800,
+                   date(1978, 3, 2))
+    print(easter)
+    print()
+
+    s = 'Rock'                                                  # demonstrate iterators
+    print('s:', s)
+    s_iterator = iter(s)
+    print('s_iterator:', s_iterator)
+    print('Iterating through s:')
+    for char in s:
+        print(next(s_iterator))
+    print()
+
+    songs_iterator = iter(songs)
+    print('songs_iterator:', songs_iterator)
+    print('Iterating through songs:')
+    for song in songs:
+        print(next(songs_iterator))
+    print()
+
+    print('Iterating through songs from album:')
+    for song in easter:
+        print(song)
+    print()
+
+    def simple_generator():                                     # demonstrate generators
+        yield("Patti Smith")
+        yield("Bruce Springsteen")
+        yield("Southside Johnny")
+        yield("...")
+
+    g = simple_generator()
+    print("Great musicians:")
+    for m in simple_generator():
+        print('\t', m)
+    print()
+
+    print("Songs on the 'Easter' album:")
+    for song in generate_songs(easter.songs):
+        print(song)
