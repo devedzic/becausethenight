061a13f2bb9bfb30adcda934737782def63ada11
devedzic <devedzic@gmail.com>
2018-10-30 22:43:23 +0100

    Cosmetic changes in settings.py.

--- becausethenight/settings.py
+++ becausethenight/settings.py
@@ -9,3 +9,8 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))            # project ro
 # print(type(PROJECT_DIR))
 # print(PROJECT_DIR)
 
+# print(__file__)
+# print(os.path.dirname(__file__))
+# print(os.path.abspath(__file__))
+# print(PROJECT_DIR)
+
